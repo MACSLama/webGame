c7d486d2a2de9a415bcae7f6ea33205ab52e68db
MACSLama <103994121+MACSLama@users.noreply.github.com>
2022-10-04 13:19:55 +0200

    sandworm

--- theGame/views.py
+++ theGame/views.py
@@ -31,16 +31,16 @@ def blue1 (request):
     
     return render (request , 'blue1/blue1.html')
 
-def blue2 (request):
+def task12 (request):
     
-    return render (request , 'blue2/blue2.html')
+    return render (request , 'blue2/task12.html')
 
-def task1 (request):
+def task11 (request):
     
     return render (request , 'blue1/task1.html')
 
 
-def task2 (request):
+def task21 (request):
      context = {}
      theScore=request.POST.get('score', None)
      deduction =  request.POST.get('helpdeduct', 0)
@@ -53,7 +53,7 @@ def task2 (request):
      return render (request , 'blue1/task2.html', {'score': score, 'truecounter':truecounter})
 
 
-def task3 (request):
+def task31 (request):
      
     context = {}
     theScore=request.POST.get('score', 0)
@@ -68,7 +68,7 @@ def task3 (request):
     return render (request , 'blue1/task3.html',  {'score': score, 'truecounter':truecounter})
 
 
-def task4 (request):
+def task41 (request):
      
     context = {}
     theScore=request.POST.get('score', 0)
@@ -83,7 +83,7 @@ def task4 (request):
     
     return render (request , 'blue1/task4.html', {'score': score, 'truecounter':truecounter})
 
-def task5 (request):
+def task51 (request):
     theScore=request.POST.get('score', 0)
     theScorAdd = request.POST.get('scoreAdd', 0)
     deduction =  request.POST.get('helpdeduct', 0)
@@ -97,7 +97,7 @@ def task5 (request):
     
     return render (request , 'blue1/task5.html', {'score': score, 'truecounter':truecounter})
 
-def task6 (request):
+def task61 (request):
 
     theScore=request.POST.get('score', 0)
     theScorAdd = request.POST.get('scoreAdd', 0)
@@ -112,7 +112,7 @@ def task6 (request):
     
     return render (request , 'blue1/task6.html', {'score': score, 'truecounter':truecounter})
 
-def task7 (request):
+def task71 (request):
     
     theScore=request.POST.get('score', 0)
     theScorAdd = request.POST.get('scoreAdd', 0)
